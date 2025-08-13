2e89c4e21a49e0cb13287b1e35e75e949932222d
Andreia Machion <amachion@gmail.com>
2025-08-13 10:49:50 -0300

    software pronto

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -1,3 +1,7 @@
-import calculadora
+import calculadora as c
 
-print (calculadora.somar(1, 2))
+print (c.somar(1, 2))
+print (c.subtrair(10, 5))
+print (c.multiplicar(2, 3))
+print (c.dividir(12, 3))
+
